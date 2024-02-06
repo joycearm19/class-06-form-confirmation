09370bfb765f10b03577888b902227ed6be2454b
Kyle J. Harms <kyle.harms@cornell.edu>
2024-02-06 17:17:44 -0500

    class 6 materials

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<header>
+  <h1 id="title"><?php echo $page_title; ?> - INFO 2300</h1>
+
+  <nav id="menu">
+    <ul>
+      <li class="<?php echo $nav_home_class; ?>"><a href="index.php">Home</a></li>
+      <li class="<?php echo $nav_citations_class; ?>"><a href="citations.php">Citations</a></li>
+      <li class="<?php echo $nav_cooking_class; ?>"><a href="cooking-classes.php">Yoko's Kitchen</a></li>
+    </ul>
+  </nav>
+</header>
